b9343bfc6f0dac28650a1c5dd9b1447d2b8ae340
alvaro <almamani@agetic.gob.bo>
2021-03-18 22:55:39 -0400

    integrando otp y qr

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <otp></otp>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Otp from './components/Otp.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {    
+    Otp
   }
 }
 </script>
